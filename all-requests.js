d3fb5caea5eaf29f2ad001ac1c948d65f9998f13
Ed Addario <ed.addario@currencycloud.com>
2019-08-07 18:10:47 +0100

    Add accounts, balances and beneficiaries

---
+++ all-requests.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019. Ed Addario. All rights reserved.
+ */
+'use strict';
+
+let currencyCloud = require('currency-cloud');
+let api = require('./lib/api')
+
+const credentials = {
+  loginId: process.argv[2],
+  apiKey: process.argv[3]
+};
+
+api.login(credentials)
+  .then(api.currentAccount)
+  .then(api.findAccounts)
+  .then(api.accountCreate)
+  .then(api.retrieveAccount)
+  .then(api.updatedAccount)
+  .then(api.findBalances)
+  .then(api.getBalance)
+  .then(api.findBeneficiaries)
+  .then(api.getBeneficiary)
+  .then(api.validateBeneficiary)
+  .then(api.createBeneficiary)
+  .then(api.updateBeneficiary)
+  .then(api.deleteBeneficiary)
+  .then(api.logout)
+  .catch((err) => {
+    if (err instanceof currencyCloud.APIerror) {
+      console.log(err.toYAML());
+    }
+    else {
+      console.log(err);
+    }
+  });
